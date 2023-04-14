42e76899339cd5c2fbb0c39093683e9be983d9df
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-04-14 18:33:29 -0300

    Conecting with API by React

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,7 +1,6 @@
 import styles from './Container.module.css';
 
 function Container(props) {
-    console.log(styles);
     return (
         <div className={`${styles.container} ${styles[props.customClass]}`}>
             {props.children}
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,33 @@
+import { useState, useEffect } from 'react';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
+
 import styles from './ProjectForm.module.css';
 
 function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((error) => console.log("Error: ", error));
+    }, []);
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Project name" name="name" placeholder="Enter with the project name" />
             <Input type="number" text="Project budget" name="budget" placeholder="Enter with the total budget" />
-            <Select name="category_id" text="Select the category" />
+            <Select name="category_id" text="Select the category" options={categories} />
             <SubmitButton text={btnText} />
         </form>
     );
