d8dde4896741f56012b10d7afd93109437242338
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-04-14 21:04:30 -0300

    Fetch projects from db

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,13 +1,17 @@
 import { useLocation } from 'react-router-dom';
 
+import { useState, useEffect } from 'react';  
+
 import Message from '../layout/Message';
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
 
 import styles from './Projects.module.css';
 
 function Projects() {
-    
+    const [projects, setProjects] = useState([]);
+
     const location = useLocation();
     console.log(location);
     let message = '';
@@ -15,6 +19,21 @@ function Projects() {
     if(location.state) {
         message = location.state.message;
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setProjects(data);
+            })
+            .catch((error) => console.log("Error: ", error));
+    }, []);
 
     return (
         <div className={styles.projectContainer}>
@@ -24,7 +43,11 @@ function Projects() {
             </div>
             {message && <Message type="success" message={message} />}
             <Container customClass="start">
-                <p>Projects...</p>
+                {projects.length > 0 && 
+                    projects.map((project) => 
+                        <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id} />
+                    )
+                }
             </Container>
         </div>
     );
